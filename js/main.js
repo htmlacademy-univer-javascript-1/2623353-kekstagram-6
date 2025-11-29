ccf99f629184e6bb1093ff4be1f9037db17f9523
Эльмира Кадырмаева <elmira1kadyrmaeva2@gmail.com>
2025-11-29 18:17:03 +0500

    большая картинка5

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 import { generatePhotos } from './photo-generator.js';
 import { renderThumbnails } from './thumbnails.js';
-import './big-picture.js';
 
 const photos = generatePhotos();
 
 document.addEventListener('DOMContentLoaded', () => {
-  renderThumbnails();
+  renderThumbnails(photos);
 });
 
 export { photos };
