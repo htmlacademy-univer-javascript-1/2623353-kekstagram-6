7bd37b43ad05f871da1009484dbc8cf148a86eeb
Keks <keks@htmlacademy.ru>
2025-11-29 15:50:43 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/big-picture.js
@@ -0,0 +1,59 @@
+const bigPicture = document.querySelector('.big-picture');
+const body = document.body;
+
+const imgElement = bigPicture.querySelector('.big-picture__img img');
+const likesCount = bigPicture.querySelector('.likes-count');
+const commentsCount = bigPicture.querySelector('.comments-count');
+const caption = bigPicture.querySelector('.social__caption');
+const commentsList = bigPicture.querySelector('.social__comments');
+const commentCountBlock = bigPicture.querySelector('.social__comment-count');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
+const closeButton = bigPicture.querySelector('.big-picture__cancel');
+
+const createCommentElement = ({ avatar, name, message }) => {
+  const li = document.createElement('li');
+  li.classList.add('social__comment');
+
+  li.innerHTML = `
+    <img class="social__picture" src="${avatar}" alt="${name}" width="35" height="35">
+    <p class="social__text">${message}</p>
+  `;
+
+  return li;
+};
+
+export function openBigPicture(photo) {
+  imgElement.src = photo.url;
+  imgElement.alt = photo.description;
+  likesCount.textContent = photo.likes;
+  commentsCount.textContent = photo.comments.length;
+  caption.textContent = photo.description;
+
+  commentsList.innerHTML = '';
+  photo.comments.forEach((comment) => {
+    commentsList.appendChild(createCommentElement(comment));
+  });
+
+  commentCountBlock.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
+
+  bigPicture.classList.remove('hidden');
+  body.classList.add('modal-open');
+
+  document.addEventListener('keydown', onEscKey);
+}
+
+export function closeBigPicture() {
+  bigPicture.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onEscKey);
+}
+
+function onEscKey(evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closeBigPicture();
+  }
+}
+
+closeButton.addEventListener('click', closeBigPicture);
