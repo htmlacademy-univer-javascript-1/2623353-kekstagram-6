18089979e9623b8140d490b870ba8f985fd922ac
Эльмира Кадырмаева <elmira1kadyrmaeva2@gmail.com>
2025-12-20 14:05:31 +0500

    Fix comments counter

--- js/api.js
+++ js/api.js
@@ -1,18 +1,18 @@
 const BASE_URL = 'https://29.javascript.htmlacademy.pro/kekstagram';
 
-const getData = () => fetch(`${BASE_URL}/data`)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error();
-    }
-    return response.json();
-  });
+const getData = () =>
+  fetch(`${BASE_URL}/data`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error();
+      }
+      return response.json();
+    });
 
 const sendData = (body) =>
   new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open('POST', BASE_URL);
-    xhr.responseType = 'json';
 
     xhr.addEventListener('load', () => {
       if (xhr.status >= 200 && xhr.status < 300) {
